d406109520213f4ca4593f014dd59e0968562431
TaylortheGenie <esampson692@gmail.com>
2023-11-03 05:41:27 +0000

    Updated functions

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -20,15 +22,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
     try:
 
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
-            model.fit(x_train, y_train) #train the model 
+            gs=GridSearchCV(model,param,cv=3)
+            gs.fit(x_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)     #train the model
+
+            logging.info("The best parameters are %s", gs.best_params_)
+            
 
             y_train_preds = model.predict(x_train)
 
@@ -44,3 +54,19 @@ def evaluate_model(x_train,y_train,x_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def scale(df,col,max,min):
+    try:
+        df[col] = (df[col]-min)/(max-min)
+        #return df
+
+    except Exception as e:
+        raise CustomException(e,sys)
